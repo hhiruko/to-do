9935699c84d638d7674564c9d4b996f118531d7d
hhiruko <adilet.toktybay@gmail.com>
2025-07-03 13:41:00 +0500

    feat: crud

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,21 @@
 import { useState } from "preact/hooks";
-import { Plus} from "lucide-preact";
+import { Plus, Pencil, PencilOff, Trash } from "lucide-preact";
 
 export function List({list}) {
     const [items, setItems] = useState(list.get());
+
+    const toggleVisibilityById = (id) => {
+        const element = document.getElementById(id);
+        const display = getComputedStyle(element).display;
+        element.style.display = display === 'block' ? 'none' : (display === 'none' ? 'block' : display);
+    };
+
+    const toggleMode = (key) => {
+        const elements = ['-input', '-label', '-edit-button', '-edit-input', '-save-button', '-delete-button'];
+        for(const element of elements) {
+            toggleVisibilityById(key + element);
+        }
+    };
 
     const addItem = () => {
         const input = document.getElementById('add-to-do-input');
@@ -20,6 +33,26 @@ export function List({list}) {
         setItems(list.get());
     }
 
+    const editItem = (key) => {
+        toggleMode(key);
+    }
+
+    const saveItem = (key) => {
+        const input = document.getElementById(key + '-edit-input');
+        const text = input.value;
+        if(text.length > 0){
+            list.edit(key, text);
+        }
+        toggleMode(key);
+        setItems(list.get());
+    }
+
+    const deleteItem = (key) => {
+        toggleMode(key);
+        list.delete(key);
+        setItems(list.get());
+    }
+
     return (
         <>
             <div id="add-to-do-container">
@@ -30,8 +63,14 @@ export function List({list}) {
             <ul class="to-do-list">
                 {Object.entries(items).sort(([a], [b]) => Number(b) - Number(a)).map(([key, item]) => (
                     <li>
-                        <input type="checkbox" id={key} onChange={() => checkItem(key)} checked={item.status}/>
-                        <label for={key}>{item.text}</label>
+                        <input type="checkbox" id={key + '-input'} onChange={() => checkItem(key)} checked={item.status}/>
+                        <label for={key + '-input'} id={key + '-label'}>{item.text}</label>
+
+                        <input type="text" value={item.text} id={key + '-edit-input'} class="edit-input"/>
+
+                        <button id={key + '-edit-button'} onClick={() => editItem(key)}><Pencil width={16} height={16} /></button>
+                        <button id={key + '-save-button'} class="save-button" onClick={() => saveItem(key)}><PencilOff width={16} height={16} /></button>
+                        <button id={key + '-delete-button'} class="delete-button" onClick={() => deleteItem(key)}><Trash width={16} height={16} /></button>
                     </li>
                 ))}
             </ul>
