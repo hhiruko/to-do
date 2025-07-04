123dfa0469ecbc1b65d60c8ae1f6e2d1c0836e5a
hhiruko <hhiruko@users.noreply.github.com>
2025-07-04 05:21:03 +0000

    deploy: 258a2215444a3b6af1ebbac0fcbf7024c74fb2ae

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,6 @@
 
     const assets = [
   "/to-do/index.html",
-  "/to-do/cdn/water.system.min.css",
-  "/to-do/cdn/water.light.min.css",
-  "/to-do/cdn/water.dark.min.css",
-  "/to-do/assets/index-rQ7h7-Px.css",
-  "/to-do/assets/index-fVbQFq8h.js",
   "/to-do/favicons/yandex-browser-manifest.json",
   "/to-do/favicons/yandex-browser-50x50.png",
   "/to-do/favicons/mstile-70x70.png",
@@ -76,9 +71,14 @@
   "/to-do/favicons/android-chrome-256x256.png",
   "/to-do/favicons/android-chrome-192x192.png",
   "/to-do/favicons/android-chrome-144x144.png",
+  "/to-do/cdn/water.system.min.css",
+  "/to-do/cdn/water.light.min.css",
+  "/to-do/cdn/water.dark.min.css",
+  "/to-do/assets/index-CvDCBGuY.css",
+  "/to-do/assets/index-B0nh94UL.js",
   "/to-do/"
 ];
-    const CACHE_NAME = 'v2025-07-03T12:10:59.568Z';
+    const CACHE_NAME = 'v2025-07-04T05:21:03.558Z';
 
     self.addEventistener('install', event => {
         event.waitUntil(
